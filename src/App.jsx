b5b6cb9a50813dd8a73baba0c6bacd03e3a9bde4
Pablo Maribondo <pablomgvieira@gmail.com>
2024-09-29 23:23:45 -0300

    feat: fetch solutions

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
+import { useEffect, useState } from 'react';
+
 function App() {
+  const [solution, setSolution] = useState(null);
+
+  useEffect(() => {
+    fetch('http://localhost:3001/solutions')
+      .then(response => response.json())
+      .then(json => {
+        const randomSolution = json[Math.floor(Math.random() * json.length)];
+        console.log(randomSolution.word);
+        setSolution(randomSolution.word);
+      });
+  }, []);
+
   return (
     <div>
       <h1>Wordle</h1>
+      {solution && <div>Solution is: {solution}</div>}
     </div>
   );
 }
