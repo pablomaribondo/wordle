4fba383916817f1680b9e5259eb2d445b1f01517
Pablo Maribondo <pablomgvieira@gmail.com>
2024-10-05 23:32:33 -0300

    feat: show past guesses

--- src/components/Grid.jsx
+++ src/components/Grid.jsx
@@ -4,7 +4,7 @@ export default function Grid({ currentGuess, guesses, turn }) {
   return (
     <div>
       {guesses.map((guess, index) => {
-        return <Row key={index} />;
+        return <Row key={index} guess={guess} />;
       })}
     </div>
   );
--- src/components/Row.jsx
+++ src/components/Row.jsx
@@ -1,4 +1,15 @@
-export default function Row() {
+export default function Row({ guess }) {
+  if (guess) {
+    return (
+      <div className="row past">
+        {guess.map((letter, index) => (
+          <div key={index} className={letter.color}>
+            {letter.key}
+          </div>
+        ))}
+      </div>
+    );
+  }
   return (
     <div className="row">
       <div></div>
